bc37f1778d3aef968509f47c095a0496438c98ab
AlejandroCdb <cordoba9809@gmail.com>
2017-05-04 16:48:11 -0500

    Update Ventas.java

--- Anuncios1.0/src/anuncios1/pkg0/Ventas.java
+++ Anuncios1.0/src/anuncios1/pkg0/Ventas.java
@@ -10,7 +10,7 @@ package anuncios1.pkg0;
  * @author Sala1
  */
 public class Ventas {
-    /*
+    
     private String cliente;
     private String caracteristicas;
     private int fecha;
@@ -18,7 +18,120 @@ public class Ventas {
     private int tipoPago;
     private int estado;
     private int saldo;
-    private int folio;*/
+    private int folio;
+    
+   /**
+     * @return the cliente
+     */
+    public String getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the caracteristicas
+     */
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    /**
+     * @param caracteristicas the caracteristicas to set
+     */
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public int getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(int fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the valor
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the tipoPago
+     */
+    public int getTipoPago() {
+        return tipoPago;
+    }
+
+    /**
+     * @param tipoPago the tipoPago to set
+     */
+    public void setTipoPago(int tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    /**
+     * @return the estado
+     */
+    public int getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public int getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the folio
+     */
+    public int getFolio() {
+        return folio;
+    }
+
+    /**
+     * @param folio the folio to set
+     */
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+    /*
     int folio;
     int opc;
     
@@ -29,4 +142,5 @@ public class Ventas {
     int eliminarVenta(int folio){
         return opc;
     }
+    */
 }
